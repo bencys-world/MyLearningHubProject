7d585268f613275993d2beb02506e621b17e50d5
bencys-world <bency.ann.arakkal@gmail.com>
2024-06-25 14:22:46 -0700

    added class to check prime number - Easy number logic category.

---
+++ src/main/java/modules/easyProblems/strings/numbers/CheckPrimeNumberTest.java
@@ -0,0 +1,37 @@
+package modules.easyProblems.strings.numbers;
+
+public class CheckPrimeNumberTest {
+
+    public static boolean isPrime(int n) {
+
+        boolean isFactor = false;
+
+        for (int i=2;i< n/2;i++){
+            if(n % i ==0) {
+                isFactor = true;
+            }
+        }
+
+        if(isFactor) {
+            return false;
+        }else {
+            return true;
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(" Is 100 a prime ? "+isPrime(100));
+
+        System.out.println(" Is 7 a prime ? "+isPrime(7));
+
+        System.out.println(" Is 71 a prime ? "+isPrime(71));
+
+        System.out.println(" Is 5 a prime ? "+isPrime(5));
+        System.out.println(" Is 9 a prime ? "+isPrime(9));
+        System.out.println(" Is 13 a prime ? "+isPrime(13));
+
+    }
+}
